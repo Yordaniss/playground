046ab846e27d563739da7d9c96f57cab48b566ee
Dmytro Werner <wernerdmytro@gmail.com>
2022-11-12 21:47:46 +0100

    backend-structure * added search endpoint

--- pages/api/posts.tsx
+++ pages/api/posts.tsx
@@ -1,19 +1,49 @@
 import clientPromise from "../../lib/mongodb";
 
 export default async (req: any, res: any) => {
-    try {
-        const client = await clientPromise;
-        const db = client.db("hackathon");
+    const method = req.method;
+    console.log(method);
+    switch (method) {
+        case 'GET':
+            try {
+                const client = await clientPromise;
+                const db = client.db("hackathon");
 
-        const posts = await db
-            .collection("posts")
-            .find({})
-            .sort({})
-            .toArray();
+                const posts = await db
+                    .collection("posts")
+                    .find({})
+                    .toArray();
 
-        res.status(200).json({ success: true, data: posts })
+                res.status(200).json({ success: true, data: posts })
 
-    } catch (e) {
-        console.error(e);
+            } catch (e) {
+                console.error(e);
+            }
+        case 'POST':
+            try {
+                const client = await clientPromise;
+                const db = client.db("hackathon");
+
+                const posts = await db
+                    .collection("posts")
+                    .aggregate([
+                        {
+                            $search: {
+                                'index': 'default',
+                                'text': {
+                                    'query': req.body.title,
+                                    'path': 'title'
+                                }
+                            },
+                        }
+                    ]).toArray();
+
+                res.status(200).json({ success: true, data: posts })
+
+            } catch (e) {
+                console.error(e);
+            }
+        default:
+            res.status(500).json({ success: false, data: 'Method not allowed.' })
     }
 };
