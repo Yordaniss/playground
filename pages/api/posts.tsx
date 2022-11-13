9091b56da49573f493ef266ab3db198547aad541
Dmytro Werner <wernerdmytro@gmail.com>
2022-11-13 10:09:38 +0100

    backend-structure * refactoring

--- pages/api/posts.tsx
+++ pages/api/posts.tsx
@@ -2,28 +2,24 @@ import clientPromise from "../../lib/mongodb";
 
 export default async (req: any, res: any) => {
     const method = req.method;
-    console.log(method);
+    const client = await clientPromise;
+    const db = client.db("hackathon");
+    
     switch (method) {
         case 'GET':
             try {
-                const client = await clientPromise;
-                const db = client.db("hackathon");
-
                 const posts = await db
                     .collection("posts")
                     .find({})
                     .toArray();
 
-                res.status(200).json({ success: true, data: posts })
+                return res.status(200).json({ success: true, data: posts })
 
             } catch (e) {
                 console.error(e);
             }
         case 'POST':
             try {
-                const client = await clientPromise;
-                const db = client.db("hackathon");
-
                 const posts = await db
                     .collection("posts")
                     .aggregate([
@@ -38,12 +34,12 @@ export default async (req: any, res: any) => {
                         }
                     ]).toArray();
 
-                res.status(200).json({ success: true, data: posts })
+                return res.status(200).json({ success: true, data: posts })
 
             } catch (e) {
                 console.error(e);
             }
         default:
-            res.status(500).json({ success: false, data: 'Method not allowed.' })
+            return res.status(500).json({ success: false, data: 'Method not allowed.' })
     }
 };
