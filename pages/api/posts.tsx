309bef9d6e6dc66d94b4a5f430a954ff3d0cfa2d
Dmytro Werner <wernerdmytro@gmail.com>
2022-11-17 13:55:47 +0100

    uploading-file * refactoring * removing comment

--- pages/api/posts.tsx
+++ pages/api/posts.tsx
@@ -4,6 +4,18 @@ import Post from "../../models/Post"
 
 const handler = nextConnect();
 
+handler.get(async (req: any, res: any) => {
+    try {
+        const posts = await Post.find({});
+
+        return res.status(200).json({ success: true, data: posts })
+
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({ success: true, data: "Internal server error" })
+    }
+});
+
 handler.use(upload.single('file'));
 
 handler.post(async (req: any, res: any) => {
@@ -22,31 +34,3 @@ export const config = {
 };
 
 export default handler;
-
-// export default async (req: any, res: any) => {
-//     const method = req.method;
-//     await dbConnect();
-
-//     switch (method) {
-//         case "GET":
-//             try {
-//                 const posts = await Post.find({});
-
-//                 return res.status(200).json({ success: true, data: posts })
-
-//             } catch (e) {
-//                 console.error(e);
-//                 return res.status(500).json({ success: true, data: "Internal server error" })
-//             }
-//         case "POST":
-//             console.log("Trying to save an object");
-//             try {
-//                 const post = await Post.create(req.body);
-//                 return res.status(201).json({ success: true, data: post })
-//             } catch (error) {
-//                 return res.status(400).json({ success: false, data: error })
-//             }
-//         default:
-//             return res.status(500).json({ success: false, data: "Method not allowed." })
-//     }
-// };
