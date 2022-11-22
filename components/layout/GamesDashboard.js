e4e048187d423c1c95656b014fdf4f520c0f98ec
Lin <118080048+lin-a-chen@users.noreply.github.com>
2022-11-22 13:35:33 +0200

    fixed svg scale issue
    Path inside svg didn't take the whole svg elements' space

--- components/layout/GamesDashboard.js
+++ components/layout/GamesDashboard.js
@@ -6,7 +6,8 @@ export default function GamesDashboard() {
       <svg
         id="svg"
         viewBox="0 0 1440 400"
-        className="svgUnderIllustration transition duration-300 ease-in-out delay-150"
+        preserveAspectRatio="none"
+        className="svgUnderIllustration"
       >
         <path
           d="M 0,400 C 0,400 0,200 0,200 C 148.92857142857142,162.28571428571428 297.85714285714283,124.57142857142858 400,131 C 502.14285714285717,137.42857142857142 557.5000000000001,188 666,218 C 774.4999999999999,248 936.1428571428571,257.42857142857144 1074,251 C 1211.857142857143,244.57142857142858 1325.9285714285716,222.28571428571428 1440,200 C 1440,200 1440,400 1440,400 Z"
@@ -18,6 +19,7 @@ export default function GamesDashboard() {
           transform="rotate(-180 720 200)"
         ></path>
       </svg>
+
       <SearchManagement></SearchManagement>
     </section>
   );
--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -3,16 +3,9 @@ import Button from "../../UI/Button";
 import React, { useState, useEffect } from "react";
 
 export default function Header() {
-  let confettiArray = [];
-  for (let i = 0; i < 50; i++) {
-    confettiArray.push(<i key={Math.random()}></i>);
-  }
-
-  const [isVisible, setIsVisible] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
 
   const scrollHandler = () => {
-    setIsVisible(false);
     setIsChecked(false);
   };
 
@@ -28,44 +21,43 @@ export default function Header() {
         src="/images/isometric-girls-room.png"
         alt="girls playing"
       />
-      <svg
-        width="100%"
-        height="100%"
-        id="svg"
-        viewBox="0 0 1440 400"
-        className="svgAboveIllustration transition duration-300 ease-in-out delay-150"
-      >
-        <path
-          d="M 0,400 C 0,400 0,200 0,200 C 305,202.5 610,205 850,205 C 1090,205 1265,202.5 1440,200 C 1440,200 1440,400 1440,400 Z"
-          stroke="none"
-          strokeWidth="0"
-          fill="#6b2c3c"
-          fillOpacity="1"
-          className="transition-all duration-300 ease-in-out delay-150 path-0"
-          transform="rotate(-180 720 200)"
-        ></path>
-      </svg>
 
       <div className="header__content">
+        <svg
+          // width="100%"
+          // height="100%"
+          id="svg"
+          viewBox="0 0 1440 400"
+          className="svgAboveIllustration"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M 0,400 C 0,400 0,200 0,200 C 305,202.5 610,205 850,205 C 1090,205 1265,202.5 1440,200 C 1440,200 1440,400 1440,400 Z"
+            stroke="none"
+            strokeWidth="0"
+            fill="#6b2c3c"
+            fillOpacity="1"
+            className="transition-all duration-300 ease-in-out delay-150 path-0"
+            transform="rotate(-180 720 200)"
+          ></path>
+        </svg>
         <input
           type="checkbox"
           id="menu-checkbox"
           onChange={(e) => {
             setIsChecked(!isChecked);
-            setIsVisible(!isVisible);
           }}
-          defaultChecked={isChecked}
+          checked={isChecked}
         ></input>
         <label htmlFor="menu-checkbox">
           <Button className="header__content__menu-btn">&#9776;</Button>
         </label>
-        {isVisible && <Navbar></Navbar>}
+        {/* <Navbar></Navbar> */}
         <div className="article">
           <div className="article__heading-container">
             <h1 className="article__heading-container__heading">
               Activities for kids
             </h1>
-            {confettiArray}
           </div>
 
           <p className="article__text">
