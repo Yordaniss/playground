61de60f0071b61db775e3f85bf7d3cde76e1d239
Lin <118080048+lin-a-chen@users.noreply.github.com>
2022-11-29 15:07:36 +0200

    Refactored scss and fixed header layout once again

--- pages/Posts.js
+++ pages/Posts.js
@@ -1,5 +1,5 @@
-import PostsDashboard from "../components/layout/postsDashboard/PostsDashboard";
+// import PostsDashboard from "../components/layout/postsDashboard/PostsDashboard";
 
-export default function Posts() {
-  return <PostsDashboard></PostsDashboard>;
-}
+// export default function Posts() {
+//   return <PostsDashboard></PostsDashboard>;
+// }
