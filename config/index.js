20867ee72ac2039747c1152d2d2f50b7b04e56c0
Lin <118080048+lin-a-chen@users.noreply.github.com>
2022-12-03 00:35:30 +0200

    modified congig file

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://playground-jet.vercel.app/";
+  : "https://playground-jet.vercel.app";
